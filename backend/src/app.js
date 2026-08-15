adff6abfd1d3d06dc4ad6e4a096fa494bad4058a
Vivek Patel <VivekPatel@gmail.com>
2026-08-15 21:51:28 +0530

    final commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ const app = express();
 ========================== */
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://stationery-r217.onrender.com",
   credentials: true
 }));
 
